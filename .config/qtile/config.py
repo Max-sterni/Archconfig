9c63acaf47d19eaed156582cfab2b2949fc58127
Max-sterni <maxvonsternber@gmail.com>
2022-06-12 22:54:57 +0200

    Revert "firefox bind"
    This reverts commit 081ca523ea77b5ec8656e659c3664c303a201459.

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -78,10 +78,9 @@ keys = [
         desc="Toggle between split and unsplit sides of stack",
     ),
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    Key([mod], "b", lazy.spawn("firefox"), desc="Open Firefox";
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawn("rofi -show drun"), desc="Spawn a command using a prompt widget"),
